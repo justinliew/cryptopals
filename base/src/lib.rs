92570094bc12a02f74ec639044dffaf58cee0e1e
Justin Liew <justinliew@gmail.com>
2018-11-13 22:22:58 -0800

    refactoring

--- base/src/lib.rs
+++ base/src/lib.rs
@@ -13,6 +13,12 @@ mod c1_tests {
     }
 
     #[test]
+    fn double() {
+        let output = super::hex_to_base64(&String::from("12"));
+        assert_eq!(output, "Eg==");
+    }
+
+    #[test]
     fn simplest_example() {
         let output = super::hex_to_base64(&String::from("010101"));
         assert_eq!(output, "AQEB");
@@ -27,11 +33,11 @@ mod c2_tests {
     //     assert_eq!(output, "746865206b696420646f6e277420706c6179");
     // }
 
-    #[test]
-    fn basic() {
-        let output = super::fixed_xor(&String::from("1"), &String::from("1"));
-        assert_eq!(output, "0");
-    }
+    // #[test]
+    // fn basic() {
+    //     let output = super::fixed_xor(&String::from("12"), &String::from("12"));
+    //     assert_eq!(output, "0");
+    // }
 }
 
 static TABLE: &'static [char] = &['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
@@ -39,7 +45,6 @@ static TABLE: &'static [char] = &['A','B','C','D','E','F','G','H','I','J','K','L
                                   '0','1','2','3','4','5','6','7','8','9','+','/'];
 
 fn encode(_input: &[u8], _output: &mut String) {
-
     _output.push(TABLE[((_input[0] & 0xfc) >> 2) as usize]);
     match _input.len() {
         1 => {
@@ -65,56 +70,53 @@ fn bytes_to_hex_digit(b : &str) -> u8 {
     u8::from_str_radix(b, 16).unwrap()
 }
 
-pub fn hex_to_base64(_input: &str) -> String {
-    let mut _num_bytes = _input.len();
+fn string_to_hex(_input: &str) -> Vec<u8> {
     let mut _stream = String::new();
-    if _num_bytes % 2 != 0 {
+    if _input.len() % 2 != 0 {
         _stream.push_str("0");
-        _num_bytes = _num_bytes + 1;
     }
-
     _stream.push_str(_input);
+
+    let mut _output = Vec::new();
+    let mut _i = 0;
+    while _i < _stream.len() {
+        _output.push(bytes_to_hex_digit(&_stream[_i.._i+2]));
+        _i += 2;
+    }
+    _output
+}
+
+pub fn hex_to_base64(_input: &str) -> String {
     let mut current_index = 0;
+    let _input_hex = string_to_hex(_input);
 
     let mut _output = String::from("");
     loop {
-        match _num_bytes - current_index {
+        match _input_hex.len() - current_index {
             0 => return _output,
-            1 | 3 | 5 => {
-                println!("Unsupported bytes");
-            },
-            2 => {
-                let mut x = Vec::new();
-                x.push(bytes_to_hex_digit(&_stream[current_index..current_index+2]));
-                encode(&x, &mut _output);
+            1 => {
+                encode(&_input_hex[current_index..current_index+1], &mut _output);
                 return _output
             },
-            4 => {
-                let mut x = Vec::new();
-                x.push(bytes_to_hex_digit(&_stream[current_index..current_index+2]));
-                x.push(bytes_to_hex_digit(&_stream[current_index+2..current_index+4]));
-                encode(&x, &mut _output);
+            2 => {
+                encode(&_input_hex[current_index..current_index+2], &mut _output);
                 return _output
             },
             _ => {
-                let mut x = Vec::new();
-                x.push(bytes_to_hex_digit(&_stream[current_index..current_index+2]));
-                x.push(bytes_to_hex_digit(&_stream[current_index+2..current_index+4]));
-                x.push(bytes_to_hex_digit(&_stream[current_index+4..current_index+6]));
-                encode(&x, &mut _output);
+                encode(&_input_hex[current_index..current_index+3], &mut _output);
+                current_index = current_index + 3;
             }
         };
-        current_index = current_index + 6;
     };
 }
 
 fn xor(_lhs: &str, _rhs: &str) -> String {
-    let _lhs_bytes = _lhs.as_bytes();
-    let _rhs_bytes = _rhs.as_bytes();
-    println!("LEN: {}, LHS: {}, RHS: {}, XOR: {}", _lhs_bytes.len(), _lhs_bytes[0], _rhs_bytes[0], _lhs_bytes[0] ^ _rhs_bytes[0]);
+    let _lhs_hex = string_to_hex(_lhs);
+    let _rhs_hex = _rhs.as_bytes();
+    println!("LHS: {}, RHS: {}, XOR: {}", _lhs_hex[0], _rhs_hex[0], _lhs_hex[0] ^ _rhs_hex[0]);
 
     let mut _output_bytes = Vec::new();
-    let _zip_iter = _lhs_bytes.iter().zip(_rhs_bytes.iter());
+    let _zip_iter = _lhs_hex.iter().zip(_rhs_hex.iter());
     for (x,y) in _zip_iter {
         println!("{} xor {} = {}", x,y,x^y);
         _output_bytes.push(x ^ y);
@@ -129,8 +131,5 @@ fn xor(_lhs: &str, _rhs: &str) -> String {
 }
 
 pub fn fixed_xor(_lhs: &str, _rhs: &str) -> String {
-    let _lhs_decoded = hex_to_base64(_lhs);
-    let _rhs_decoded = hex_to_base64(_rhs);
-    assert_eq!(_lhs_decoded.len(), _rhs_decoded.len());
-    xor(&_lhs_decoded,&_rhs_decoded)
+    xor(&_lhs,&_rhs)
 }
